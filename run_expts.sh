d19f97a54c5ef31ab47fa408cad236bef44da3ec
Scott Campit <scottcampit@gmail.com>
2023-06-26 23:22:28 -0700

    Push yolov7 up one directory

--- run_expts.sh
+++ run_expts.sh
@@ -1,2 +1,12 @@
 # Run baseline yolov7
-python3 ./experiments/baseline/inference.py
+python3 ./couro/experiments/baseline/inference.py
+
+# Fine-tuning for running phase detection
+python3 ./couro/experiments/running_phase_detection/train.py \
+    --epochs 100 \
+    --workers 4 \ 
+    --device 0 \ 
+    --batch-size 16 \
+    --data 
+
+# Fine-tuning for action recognition
