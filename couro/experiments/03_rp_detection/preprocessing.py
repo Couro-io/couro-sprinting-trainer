7853c37ecf9adc6b14f7643bfaf0bfb0604799b8
Scott Campit <scottcampit@gmail.com>
2023-07-10 21:42:07 -0700

    Add validation set preprocessing code

--- couro/experiments/03_rp_detection/preprocessing.py
+++ couro/experiments/03_rp_detection/preprocessing.py
@@ -18,11 +18,6 @@ import torch
 sys.path.append('./visualize')
 from visualize import get_cvat_bbox, remove_img_with_no_annot
 
-def get_files_with_annotations(path_to_annotations:str) -> List[str]:
-    """
-    """
-    pass
-
 def create_data_splits(image_files:list, label_files:list, train_ratio:float=0.70, test_ratio:float=0.15, val_ratio:float=0.15):
     """
     """
@@ -54,8 +49,8 @@ def calc_x_y_center(xtl:float, ytl:float, xbr:float, ybr:float) -> Tuple[float, 
     """
     Calculate the center of a bounding box.
     """
-    x_center = (xbr + xtl) / 2
-    y_center = (ybr + ytl) / 2
+    x_center = (float(xbr) + float(xtl)) / 2
+    y_center = (float(ybr) + float(ytl)) / 2
     return x_center, y_center
 
 def get_key_from_value(dictionary, value):
@@ -64,7 +59,7 @@ def get_key_from_value(dictionary, value):
             return key
     return None
 
-def extract_label(data: dict, save_prefix:str, label_encoding_path:str='./data/processed.json', data_split:str='./data/processed/train/'):
+def extract_label(data: dict, save_prefix:str, label_encoding_path:str='./data/processed/label_dict.json.json', data_split:str='./data/processed/train/'):
     """
     labels should be named for the corresponding video_img_num combination with the following format:
     class x_center y_center width height
@@ -73,36 +68,52 @@ def extract_label(data: dict, save_prefix:str, label_encoding_path:str='./data/p
     with open(label_encoding_path, 'r') as file:
         label_encoding_dict = json.load(file)
     
+    image_filenames = []
     for image_filename, annotations in data.items():
-        new_filename = f"{save_prefix}_{image_filename.split('.')[0]}.txt"
-        with open(os.path.join(data_split, new_filename), 'w') as file:
+        image_filenames.append(image_filename)
+        filename_without_extension = image_filename.split('.')[0]
+        new_filename = f"{save_prefix}_{filename_without_extension}.txt"  
+        with open(os.path.join(data_split, 'labels', new_filename), 'w') as file:
             for annotation in annotations:
                 x_center, y_center = calc_x_y_center(annotation['xtl'], annotation['ytl'], annotation['xbr'], annotation['ybr'])
                 width = annotation['width']
                 height = annotation['height']
                 label = get_key_from_value(label_encoding_dict, annotation['label'])
                 
-                # Format the annotation as "class x_center y_center width height"
                 formatted_annotation = f"{label} {x_center} {y_center} {width} {height}"
                 
-                # Write the formatted annotation to the file
                 file.write(formatted_annotation + '\n')
-                print(new_filename)
-                print(formatted_annotation)
-
-def rename_img(img_name:str, save_prefix:str, input_data:str="./data/raw/", output_data_split:str="./data/processed/train/"):
-    """
-    """
-    image = PIL.Image.open(os.path.join(data_split, img_name))
-    new_filename = f"{save_prefix}_{img_name.split('.')[0]}.jpg"
+    return image_filenames
     
+def get_files_with_annotations(path_to_annotations:str, \
+        path_to_img:str, \
+        save_prefix:str, \
+        label_encoding_path:str='./data/processed/label_dict.json', \
+        data_split:str='./data/processed/train/') -> List[str]:
+    current_date = datetime.now()
+    unique_id = uuid.uuid4()
+    logname = f"{current_date.strftime('%Y%m%d%H%M%S')}_{unique_id}"
     
-def create_img_label_dataset():
-    """
-    """
-    pass
+    image_filenames = []
+    
+    for xml in os.listdir(path_to_annotations):
+        xml_path = os.path.join(path_to_annotations, xml)
+        label_map = get_cvat_bbox(xml_path)
+        filtered_map = remove_img_with_no_annot(label_map, logname)
+        image_filenames.extend(extract_label(filtered_map, save_prefix, label_encoding_path, data_split))
+    print(len(image_filenames))
+    
+    tmp_new_filenames = list()    
+    for img in os.listdir(path_to_img):
+        if img in image_filenames:
+            new_filename = f"{save_prefix}_{img}"
+            image = PIL.Image.open(os.path.join(path_to_img, img))
+            image.save(os.path.join(data_split, 'images', new_filename))
+            tmp_new_filenames.append(new_filename)
+    print(len(tmp_new_filenames))
 
 if __name__ == '__main__':
+    """
     xml_path = "./annotations/train/annotations 1.xml"
     current_date = datetime.now()
     unique_id = uuid.uuid4()
@@ -110,6 +121,10 @@ if __name__ == '__main__':
     label_dict = get_cvat_bbox(xml_path)
     annot_dict = remove_img_with_no_annot(label_dict, logname)
     pprint(annot_dict)
+    """
+    
+    get_files_with_annotations(path_to_annotations="./annotations/validation/", path_to_img="./data/raw/validation/video 3", save_prefix="video3", data_split="./data/processed/validation/")
+    
 
     
     
