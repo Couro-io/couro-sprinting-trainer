f17ebc50c3af4fb09abb95eeab590718165b30de
Scott Campit <scottcampit@gmail.com>
2023-07-11 18:27:20 -0700

    Add label encoding dict as object

--- couro/experiments/03_rp_detection/tests/conftest.py
+++ couro/experiments/03_rp_detection/tests/conftest.py
@@ -3,6 +3,7 @@ Store fixtures for tests
 """
 
 import pytest
+import json
 
 @pytest.fixture
 def example_annotation() -> dict:
@@ -19,3 +20,10 @@ def example_annotation() -> dict:
         'height': '200',
     }
     
+@pytest.fixture
+def label_encoding_dict() -> dict:
+    """Label encoding dictionary"""
+    label_encoding_path='./../data/processed/label_dict.json'
+    with open(label_encoding_path, 'r') as file:
+        label_encoding_dict = json.load(file)
+    return label_encoding_dict
